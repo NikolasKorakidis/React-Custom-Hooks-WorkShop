1e0f8169208247a7f34cee605a351214d9443bc3
nikolaskorakidis <korakidisn@gmail.com>
2021-01-07 13:37:39 +0100

    Settup of the main content

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+// src/components/Message.js
+import React from "react";
+
+export default function Message({ text, sender, sentAt }) {
+  return (
+    <div>
+      <small>
+        <strong>{sender}</strong> {sentAt}
+      </small>
+      <p>{text}</p>
+    </div>
+  );
+}
